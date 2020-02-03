72e5bd1f8419c507590dc138b2190f91316c6508
asu <sdelang@sdelang.fr>
2020-02-03 09:23:52 +0100

    Handle exceptions in main gracefully

--- src/smol/asm/main.cpp
+++ src/smol/asm/main.cpp
@@ -10,23 +10,31 @@
 
 auto main(int argc, char** argv) -> int
 {
-	const std::vector<std::string_view> args(argv + 1, argv + argc);
-
-	if (args.size() != 1)
+	try
 	{
-		fmt::print(stderr, "Syntax: ./smolisa-as <source>\nBinaries are written on stdout\n");
+		const std::vector<std::string_view> args(argv + 1, argv + argc);
+
+		if (args.size() != 1)
+		{
+			fmt::print(stderr, "Syntax: ./smolisa-as <source>\nBinaries are written on stdout\n");
+			return 1;
+		}
+
+		const auto source_path = args[0];
+		const auto source      = load_file_raw(source_path);
+		Assembler  assembler{source.data()};
+
+		for (Byte byte : assembler.program_output)
+		{
+			std::cout.put(byte);
+		}
+
+		// TODO: allow to put arbitrary bytes
+		// TODO: allowing to load low and high bits of address!!
+	}
+	catch (const std::exception& e)
+	{
+		fmt::print(stderr, "Assembler exited: {}\n", e.what());
 		return 1;
 	}
-
-	const auto source_path = args[0];
-	const auto source      = load_file_raw(source_path);
-	Assembler  assembler{source.data()};
-
-	for (Byte byte : assembler.program_output)
-	{
-		std::cout.put(byte);
-	}
-
-	// TODO: allow to put arbitrary bytes
-	// TODO: allowing to load low and high bits of address!!
 }
